8c173d63e19a5e972ac86cb619e6bb07e7c46f2a
Ruan Pereira Mattos <ruandupg@gmail.com>
2025-05-23 15:23:11 -0300

    Implementation of onResume method, when the user pauses, stops or whatever, when the app is resumed, the counter value change to 0 and the counting start over

--- app/src/main/java/com/raiovack/singlepageapp/Main.kt
+++ app/src/main/java/com/raiovack/singlepageapp/Main.kt
@@ -8,9 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Main : AppCompatActivity(R.layout.act_main) {
 
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var counter = 0
         val linearLayout: LinearLayout = findViewById(R.id.linear_layout)
         val title: TextView = findViewById(R.id.title)
 
@@ -20,6 +21,11 @@ class Main : AppCompatActivity(R.layout.act_main) {
         }
 
         linearLayout.setOnClickListener(clickListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        counter = 0
     }
 
 }
