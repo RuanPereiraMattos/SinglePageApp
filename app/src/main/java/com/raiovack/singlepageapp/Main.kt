ca55dfec16ac46cf0fb91526ec5bce97450a2e64
Ruan Pereira Mattos <ruandupg@gmail.com>
2025-05-23 15:01:00 -0300

    Main.kt was changed as well with addiction of "onCreate" method from AppCompatActivity and inside this method was created linearLayout variable and title as final variables, was created clickListener variable as object to do the click event in linearLayout that only change title text to "Well done! Button Clicked!" and last thing clickListener was call inside linearLayout with setOnClickListener so the click event can be trigered

--- app/src/main/java/com/raiovack/singlepageapp/Main.kt
+++ app/src/main/java/com/raiovack/singlepageapp/Main.kt
@@ -1,6 +1,25 @@
 package com.raiovack.singlepageapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Main : AppCompatActivity(R.layout.act_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val linearLayout: LinearLayout = findViewById(R.id.linear_layout)
+        val title: TextView = findViewById(R.id.title)
+
+        val clickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                title.text = "Well done! Button Clicked!"
+            }
+        }
+
+        linearLayout.setOnClickListener(clickListener)
+    }
+
 }
